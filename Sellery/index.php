c57337fb9fb6057ba6d8819fef3894afd1ab9866
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-01-31 08:43:32 -0600

    Update index.php

--- Sellery/index.php
+++ Sellery/index.php
@@ -8,7 +8,9 @@
     <title>Test Database</title>
 	<meta name="google-signin-scope" content="profile email">
 	<meta name="google-signin-client_id" content="823261360761-fbpr77vju6iv8btqqrhkol438b3gd7e3.apps.googleusercontent.com">
-  </head>
+  	<script src="https://apis.google.com/js/platform.js" async defer></script>
+		<script src="js/g+login.js" type="text/javascript" ></script>
+	</head>
   <body>
     Testing
   </body>
