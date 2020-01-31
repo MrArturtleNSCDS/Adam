87790f02c279b185db7ed3436abc80fb7e50db04
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-01-31 08:40:54 -0600

    Update index.php

--- Sellery/index.php
+++ Sellery/index.php
@@ -6,6 +6,8 @@
 <html lang="en">
 	<head>
     <title>Test Database</title>
+	<meta name="google-signin-scope" content="profile email">
+	<meta name="google-signin-client_id" content="823261360761-fbpr77vju6iv8btqqrhkol438b3gd7e3.apps.googleusercontent.com">
   </head>
   <body>
     Testing
