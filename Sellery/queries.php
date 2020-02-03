f925d1869ddf6b680dc8813d18fa5bd7d1e38e90
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-02-03 10:42:54 -0600

    Create queries.php

---
+++ Sellery/queries.php
@@ -0,0 +1,33 @@
+$userid = $_POST['userid'];
+        $skillid = $_POST['skillid'];
+        $newbucket = $_POST['newbucket'];
+        
+        $getCurrentSkillString = 
+            "SELECT bucket,date FROM skillsusermatch
+            WHERE userid=$userid AND skillid=$skillid";
+        
+        $getCurrentSkillQuery = query($getCurrentSkillString, $dbHandle);
+        
+        
+        $updateSkillString = 
+            "INSERT INTO skillsusermatch (userid,skillid,bucket,date) 
+            VALUES ($userid,$skillid,$newbucket,NOW())";
+        
+        if($getSkillResult=$getCurrentSkillQuery->fetch_array()){
+            $oldBucket = $getSkillResult['bucket'];
+            $oldDate = $getSkillResult['date'];
+            
+            $updateSkillString = 
+                "UPDATE skillsusermatch SET bucket=$newbucket,date=NOW()
+                WHERE userid=$userid AND skillid=$skillid";
+            
+            $insertHistoryString = 
+                "INSERT INTO skillshistory (userid,skillid,bucket,date) 
+                VALUES ($userid,$skillid,$oldBucket,'$oldDate')";
+            
+                $insertHistoryQuery = query($insertHistoryString, $dbHandle);
+        }
+
+        $updateSkillQuery = query($updateSkillString,$dbHandle);
+        
+        echo 1;
