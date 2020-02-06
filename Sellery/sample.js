d7eda45ca8b3378cb09e416c345ff238476d980c
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-02-06 15:03:33 -0600

    Update sample.js

--- Sellery/sample.js
+++ Sellery/sample.js
@@ -26,3 +26,44 @@ function changeSkill(skillElement,newBucket){
         alert( "Request failed: " + textStatus + " " + jqXHR.responseText);
     });
 }
+
+
+function populateTeamRoster(teamid_){
+    console.log("populating teamid:" + teamid_);
+    $('.playerContainer').html('');
+    
+    var requestRoster = $.ajax({
+        url: "php/queries.php",
+        type: "POST",
+        data: {action:'getRoster',teamid:teamid_},
+        dataType: "xml"
+    });
+
+    requestRoster.done(function(teamXML){
+        allPlayers = $(teamXML).find('player');
+        console.log("total players:" + allPlayers.length);
+        
+        allPlayers.each(
+            function(){
+                var playerId = $(this).find('id').text();
+                var playerName = $(this).find('name').text();
+                var playerStatus = $(this).find('status').text();
+                var playerPlays = $(this).find('plays').text();
+                
+                $('[container=' + playerStatus + ']').append(
+                    "<div class='playerWrapper' playerid='" + playerId + "'>" + 
+                        "<div class='playerName'>" + playerName + "</div>" +
+                        "<div class='numPlays'>" + playerPlays + "</div>" +
+                        "<div class='buttonTransparent add'></div>" +
+                        "<div class='buttonTransparent remove'></div>" +
+                    "</div>");
+            }
+        );
+        activatePlayerButtons();
+        updateNumPlayers();
+    });
+
+    requestRoster.fail(function(jqXHR, textStatus) {
+        alert( "Request failed: " + textStatus + " " + jqXHR.responseText);
+    });
+}
