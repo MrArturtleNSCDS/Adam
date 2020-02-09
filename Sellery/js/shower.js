f0585b61c125a2ce5c75872b63459c4f60e5c226
ATerhaerdt <52171012+ATerhaerdt@users.noreply.github.com>
2020-02-09 14:25:09 -0600

    Update shower.js

--- Sellery/js/shower.js
+++ Sellery/js/shower.js
@@ -23,7 +23,7 @@ function showElements(element){
     $(element).delay(1800).fadeIn();
 }
 function home(){
-    transition();
+    //transition();
     showElements(registry);
     hideElements(contact);
     hideElements(other);
@@ -31,7 +31,7 @@ function home(){
     hideElements(info);
 }
 $("#home1").click(function(){
-    transition();
+    //transition();
     showElements(registry);
     hideElements(contact);
     hideElements(other);
@@ -39,7 +39,7 @@ $("#home1").click(function(){
     hideElements(info);
 });
 $('#info1').click(function(){
-    transition();
+    //transition();
     hideElements(registry);
     hideElements(contact);
     hideElements(other);
@@ -47,7 +47,7 @@ $('#info1').click(function(){
     showElements(info);
 });
 $('#contact1').click(function(){
-    transition();
+    //transition();
     hideElements(registry);
     showElements(contact);
     hideElements(other);
@@ -55,7 +55,7 @@ $('#contact1').click(function(){
     showElements(info);
 });
 $('#logout1').click(function logOut(){
-    transition();
+    //transition();
     hideElements(registry);
     showElements(contact);
     hideElements(other);
@@ -66,38 +66,14 @@ $('#logout1').click(function logOut(){
       console.log('User signed out.');
     });
 });
-
-
-
+/*
 function transition(){
     $("#bar1").animate({height:"100%"},1500).delay(100).animate({height:0},2000);
     $("#bar2").delay(200).animate({height:"100%"},1500).delay(100).animate({height:0},2000);
     $("#bar3").delay(400).animate({height:"100%"},1500).delay(100).animate({height:0},2000);
     $("#bar4").delay(600).animate({height:"100%"},1500).delay(100).animate({height:0},2000);
 }
-
-function names(first_, last_, email_){
-    function checkInfo(){
-        var requestName = $.ajax({
-            url: "queries.php",
-            type: "POST",
-            data: {action:"check",first:first_, last:last_, email:email_},
-            dataType: "text"
-        });
-
-        requestName.done(function(success){
-        // if(success==="1"){
-                console.log(success);
-            //}
-        });
-
-        requestName.fail(function(jqXHR, textStatus) {
-            alert( "Request failed: " + textStatus + " " + jqXHR.responseText);
-        });
-    }
-}
-
-
+*/
 function getName(){
     var firstName = $("#first").val();
     var lastName = $("#last").val();
@@ -129,3 +105,60 @@ function getName(){
 
 }
 
+function getInfo(){
+    var userId = $("#user").val();
+    var getInfo = $.ajax({
+        url: "queries.php",
+        type: "POST",
+        data: {action:"recieve",user:userId},
+        dataType: "xml"
+    });
+
+    getInfo.done(function(userXML){
+        allUsers = $(userXML).find('user');
+        console.log("Total Users:" + allUsers.length);
+
+        allUsers.each(function(){
+            var userFirst = $(this).find('first').text();
+            var userLast = $(this).find('last').text();
+            var userStall = $(this).find('stall').text();
+            var userStartTime = $(this).find('start').text();
+            var userEndTime = $(this).find('end').text();
+            var minutes = $(this).find('minutes').text();
+            
+            console.log(minutes);
+
+            $('#container').append(
+                    "<div class='userFirst'>" + userFirst + "</div><br>" +
+                    "<div class='userLast'>" + userLast + "</div><br>" +
+                    "<div class='userStall'>" + userStall + "</div><br>" +
+                    "<div class='userStartTime'>" + userStartTime + "</div><br>" +
+                    "<div class='userEndTime'>" + userEndTime + "</div><br>" +
+                    "<div class='userEndTime'>" + minutes + "</div><br>");
+        })
+
+    });
+
+    getInfo.fail(function(jqXHR, textStatus) {
+        alert( "Request failed: " + textStatus + " " + jqXHR.responseText);
+    });
+}
+
+function checkInfo(first_, last_, email_){
+    var checkI = $.ajax({
+        url: "queries.php",
+        type: "POST",
+        data: {action:"check",firstCheck:first_, lastCheck:last_, emailCheck:email_},
+        dataType: "text"
+    });
+
+    checkI.done(function(success){
+       // if(success==="1"){
+            console.log(success);
+        //}
+    });
+
+    checkI.fail(function(jqXHR, textStatus) {
+        alert( "Request failed: " + textStatus + " " + jqXHR.responseText);
+    });
+}
