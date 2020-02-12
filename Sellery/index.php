d3fecbbc996679cdaea31d7b5c31c0f0414432c9
Arturo Garcia <agarcia@nscds.org>
2020-02-12 14:33:42 -0600

    Updated

--- Sellery/index.php
+++ Sellery/index.php
@@ -5,17 +5,21 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="js/jquery-3.4.1.min.js"></script>
         <script src="https://kit.fontawesome.com/f5406f8a6e.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
+        <link href="images/WisconsinShower.png" type="image/png" rel='icon'>
         <meta name="google-signin-scope" content="profile email">
         <meta name="google-signin-client_id" content="153584192754-qbp9uaam50pg8slsps3k0ig7t5u8788k.apps.googleusercontent.com">
         <script src="https://apis.google.com/js/platform.js" async defer></script>
-        <script src="Js/g+login.js" type="text/javascript" ></script>
-        <link rel= 'stylesheet' href= 'CSS/shower.css'>
+        <script src="js/g+login.js" type="text/javascript" ></script>
+        <link rel= 'stylesheet' href= 'css/shower.css'>
+        <link rel= 'stylesheet' href= 'css/timepicker.min.css'>
+        <script src='js/timepicker.min.js'></script>
         <title>Wisconsin Shower Sellery</title>
     </head>
     <body>
@@ -26,12 +30,12 @@
                 <div id = "bar3" class = bara></div>
                 <div id = "bar4" class = barb></div>
             </div>
-            <img src="images/Madison.jpg" class= "campusPic" alt="campusP" width=100% height=20%>
-            <div id="sidebar1">
+            <div id="header">
+                <img src="images/Madison.jpg" class= "campusPic" alt="campusP">
                 <div class="sidebar">
-                    <div id = "home1"><a href="#home"><i class="fas fa-home"></i> Home</a></div>
-                    <div id = "contact1"><a href="#contact"><i class="fas fa-envelope"></i> Contact</a></div>
-                    <div id = "logout1"><a href="#logout"><i class="fas fa-sign-in-alt"></i><br>LogOut</a></div>
+                    <div id = "home1"><a href="#home"><i class="fas fa-home"></i></a></div>
+                    <div id = "contact1"><a href="#contact"><i class="fas fa-envelope"></i></a></div>
+                    <div id = "logout1"><a href="#logout"><i class="fas fa-sign-in-alt"></i></a></div>
                 </div>
             </div>
             <div id='logWrapper'>
@@ -51,15 +55,15 @@
                     <div id="fname" class='name allCheckBoxes' style="font-size:5vw;"></div>
                     <div id="lname" class= 'name allCheckBoxes' style="font-size:3vw;"></div>
                     <div id='switchboygirl'>
-                        <div id="areYou" class='Text' style="font-size:4vw;">What are you?</div>
-                        <div id='boy' class='Text boygirl' style="font-size:3vw;">Boy</div>
+                        <div id="areYou" class='Text' style="font-size:4vw;">Are you?</div>
+                        <div id='boy' class='Text boygirl' style="font-size:3vw;">Male</div>
                         <label class="switch">
                             <input id = "genderCheckBox" type="checkbox">
                             <span class="slider round"></span>
                         </label>
-                        <div id='girl' class='Text boygirl' style="font-size:3vw;">Girl</div>
+                        <div id='girl' class='Text boygirl' style="font-size:3vw;">Female</div>
                     </div>
-                    <div id="timeE">
+                    <!--<div id="timeE">
                         <div id="timeExpected" class='Text' style="font-size:4vw;">How Long Does Your Shower Usually Take?</div>
                         <div id="radioButton">
                             <label class="container Text" style="font-size:3vw;">One Minute
@@ -83,28 +87,28 @@
                                 <span class="checkmark"></span>
                             </label>
                         </div>
-                    </div>
+                    </div>-->
                     <button id="sumbitB" button class="fsSubmitButton" onClick = insert();homey() style="font-size:5vw;">Submit</button>
                 </div>
 
                 <div id="home">
                     <div id = "hello" class="Text" style="font-size:6vw;">Hello</div>
                     <div class="grid-container">
-                        <div class="grid-item" style="font-size:3vw;">1</div>
-                        <div class="grid-item" style="font-size:3vw;">2</div>
-                        <div class="grid-item" style="font-size:3vw;">3</div>
-                        <div class="grid-item" style="font-size:3vw;">4</div>
-                        <div class="grid-item" style="font-size:3vw;">5</div>
-                        <div class="grid-item" style="font-size:3vw;" stall='1'></div>
-                        <div class="grid-item" style="font-size:3vw;" stall='2'></div>
-                        <div class="grid-item" style="font-size:3vw;" stall='3'></div>
-                        <div class="grid-item" style="font-size:3vw;" stall='4'></div>
-                        <div class="grid-item" style="font-size:3vw;" stall='5'></div>
-                        <button id = "stall1" class = "button1" stallButton="1" onclick="document.getElementById('id01').style.display='block'">Sign in on Stall 1</button>
-                        <button id = "stall2" class = "button1" stallButton="2" onclick="document.getElementById('id01').style.display='block'">Sign in on Stall 2</button>
-                        <button id = "stall3" class = "button1" stallButton="3" onclick="document.getElementById('id01').style.display='block'">Sign in on Stall 3</button>
-                        <button id = "stall4" class = "button1" stallButton="4" onclick="document.getElementById('id01').style.display='block'">Sign in on Stall 4</button>
-                        <button id = "stall5" class = "button1" stallButton="5" onclick="document.getElementById('id01').style.display='block'">Sign in on Stall 5</button>
+                        <div class="grid-item grid-header" style="font-size:3vw;">1</div>
+                        <div class="grid-item grid-header" style="font-size:3vw;">2</div>
+                        <div class="grid-item grid-header" style="font-size:3vw;">3</div>
+                        <div class="grid-item grid-header" style="font-size:3vw;">4</div>
+                        <div class="grid-item grid-header" style="font-size:3vw;">5</div>
+                        <div class="grid-item grid-stall scroll" style="font-size:3vw;" stall='1'></div>
+                        <div class="grid-item grid-stall scroll" style="font-size:3vw;" stall='2'></div>
+                        <div class="grid-item grid-stall scroll" style="font-size:3vw;" stall='3'></div>
+                        <div class="grid-item grid-stall scroll" style="font-size:3vw;" stall='4'></div>
+                        <div class="grid-item grid-stall scroll" style="font-size:3vw;" stall='5'></div>
+                        <button id = "stall1" class = "button1" stallButton="1">Sign up</button>
+                        <button id = "stall2" class = "button1" stallButton="2">Sign up</button>
+                        <button id = "stall3" class = "button1" stallButton="3">Sign up</button>
+                        <button id = "stall4" class = "button1" stallButton="4">Sign up</button>
+                        <button id = "stall5" class = "button1" stallButton="5">Sign up</button>
                     </div>
                     <div class="drop"></div>
                 </div>
@@ -117,30 +121,25 @@
                 </div>
             </div>
         </div>
+        
         <div id="id01" class="w3-modal">
-            <div class="w3-modal-content w3-modal-content w3-animate-zoom">
+            <div class="w3-modal-content">
+                <header class="w3-container modalHeader"> 
+                    <span onclick="document.getElementById('id01').style.display='none'" 
+                        class="w3-button w3-display-topright">&times;</span>
+                    <h2>Pick a time for stall <span id='modalStallNum'></span></h2>
+                </header>
                 <div class="w3-container">
-                <span onclick="document.getElementById('id01').style.display='none'"
-                class="w3-button w3-display-topright">&times;</span>
-                <p>Start</p>
-                <input id = "hours" type = "text" class= "in-linea" placeholder = "H" style = "width:3vh;height:3vh;background-color:#C4012E">:</input>
-                <input id = "minutes" type = "text" class= "in-linea" placeholder = "Min" style = "width:4vh;height:3vh;background-color:#C4012E"></input>
-                <input id = "duration" type = "text" class= "in-linea" placeholder = "Duration" style = "width:10vh;height:3vh;background-color:#C4012E"></input>
-                <div id = "sumbitM">Sumbit</div>
-                <div id="radioButtonModal">
-                            <label class="container modalRadioText" style="font-size:3vw;">AM
-                                <input type="radio" checked="checked" name="AMPM">
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="container modalRadioText animate" style="font-size:3vw;">PM
-                                <input id = "PM" type="radio" name="AMPM">
-                                <span class="checkmark"></span>
-                            </label>
+                    <div>Time <input class='timepicker' readonly></div>
+                    <div>Duration <input class='durationpicker' readonly></div>
+                </div>
+                <footer class="w3-container modalHeader">
+                    <button id='submitM'>Submit</button>
+                </footer>
             </div>
-         </div>
-</div>
+        </div>
     
     </body>
     <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
-    <script src='Js/shower.js'></script>
+    <script src='js/shower.js'></script>
 </html>
